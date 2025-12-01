9d43fa39de5c9ca92012621fb275501a2cb71624
EnrikPaulo <enrikp.2016@gmail.com>
2025-12-01 18:40:50 -0300

    JwtStrategy e JwtAuthGuard concluidos

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { 
+  Controller, 
+  Post, 
+  Body, 
+  BadRequestException 
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  // 🔹 Registrar administrador
+  @Post('register')
+  async register(
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ) {
+    if (!email || !password) {
+      throw new BadRequestException('E-mail e senha são obrigatórios');
+    }
+
+    return this.authService.register(email, password);
+  }
+
+  // 🔹 Login
+  @Post('login')
+  async login(
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ) {
+    if (!email || !password) {
+      throw new BadRequestException('E-mail e senha são obrigatórios');
+    }
+
+    return this.authService.login(email, password);
+  }
+}
+
